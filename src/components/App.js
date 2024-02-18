38bfd9aca5674a1d6659bbf7e0e2a95ae4f025a2
Allan <allan.kariuki@student.moringaschool.com>
2024-02-19 02:22:09 +0300

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+import React, { useState, useEffect } from "react";
+
+function App() {
+    const [imageFetch, setImageFetch] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    const apiUrl = "https://dog.ceo/api/breeds/image/random";
+    useEffect(() => {
+        fetch(apiUrl)
+            .then((res) => res.json())
+            .then((data) => {
+                setImageFetch(data.message);
+                setIsLoaded(true);
+                // console.log(data);
+            });
+    }, []);
+
+    if (!isLoaded) return <p>Loading...</p>;
+
+    return (
+        <div>
+            <img src={imageFetch} alt="A Random Dog"></img>
+        </div>
+    );
+}
+
+export default App;
